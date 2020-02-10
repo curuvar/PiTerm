142b0d3c46b962686d1d355f2fa464340d44f456
Craig Altrenburg <craig@qrpc.com>
2020-02-09 19:22:11 -0500

    Initial Commit

---
+++ PiTerm/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PiTerm
+//
+//  Created by Craig Altenburg on 2020/02/09/.
+//  Copyright © 2020 Craig Altenburg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
